d8830d3e870d67583ff596b0e2b5d8d2821e3d39
sudhakar mishra <wad@avanthabsl.com>
2021-04-26 21:02:40 +0530

    temperature changes

--- src/app/temperature/temperature.component.ts
+++ src/app/temperature/temperature.component.ts
@@ -12,6 +12,11 @@ export class TemperatureComponent implements OnInit {
 
     tempForm: FormGroup;
     showError: boolean;
+    min: number;
+    max: number;
+    mean: number;
+    mode: number;
+    temperatureData:any;
 
     constructor(private route: ActivatedRoute,private toastr: ToastrService,
         private formBuilder: FormBuilder,
@@ -26,27 +31,65 @@ export class TemperatureComponent implements OnInit {
 
     initializeFormControl() {
         this.tempForm = this.formBuilder.group({
-          'number': [null, [Validators.required]]
+          'temperature': [null, [Validators.required]]
         });
       }
 
     onSubmit() {
         this.showError = true;
         const formValue = this.tempForm.value;
-        if (this.validateForm(formValue)) {
             this.showError = false;
             this.tempForm.reset();
+    }
+
+
+    insert(num: number): any {
+          if (num > 0 && num < 150) {
+              this.temperatureData.push(num);
+              this.min = Math.min.apply(null, this.temperatureData);
+              this.max = Math.max.apply(null, this.temperatureData);
+              this.mean = Number(this.meanTemperature().toFixed(2));
+              this.mode = this.calculateMode();
+              return this.temperatureData;
+            }
+            else {
+              return 'Temperature should be integer';
+            }
+          }
+        
+      
+
+      meanTemperature(): number {
+        let totalSum = 0;
+        this.temperatureData.forEach((element) => {
+          totalSum += element;
+        });
+        return totalSum / this.temperatureData.length;
+      }
+
+
+      calculateMode(): number {
+        let mode = 0;
+        for (let i = 0; i < this.temperatureData.length; i++) {
+          for (let j = 0; j < i; j++) {
+            if (this.temperatureData[j] === this.temperatureData[i]) {
+              mode = this.temperatureData[j];
+            }
+          }
         }
+        return mode;
+      }
+
+      getMin(): number {
+        return this.min;
+      }
+      getMax(): number {
+        return this.max;
+      }
+      getMean(): number {
+        return this.mean;
+      }
+      getMode(): number {
+        return this.mode;
+      }
     }
-
-    validateForm(values){
-        if (!values.firstName) {
-            return false;
-          } else if (!values.lastName) {
-            return false;
-          } 
-          return true;
-    }
-
-
-  }
