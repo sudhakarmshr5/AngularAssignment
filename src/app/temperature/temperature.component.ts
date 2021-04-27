09519e92e976a4cc0a8aca392c051a1502c0c1c8
sudhakar mishra <wad@avanthabsl.com>
2021-04-27 10:37:00 +0530

    validation changes

--- src/app/temperature/temperature.component.ts
+++ src/app/temperature/temperature.component.ts
@@ -16,9 +16,9 @@ export class TemperatureComponent implements OnInit {
     max: number;
     mean: number;
     mode: number;
-    temperatureData:any;
+    temperatureData: any;
 
-    constructor(private route: ActivatedRoute,private toastr: ToastrService,
+    constructor(private route: ActivatedRoute, private toastr: ToastrService,
         private formBuilder: FormBuilder,
         private router: Router) {
         this.initializeFormControl();
@@ -31,65 +31,66 @@ export class TemperatureComponent implements OnInit {
 
     initializeFormControl() {
         this.tempForm = this.formBuilder.group({
-          'temperature': [null, [Validators.required]]
+            'temperature': [null, [Validators.required]]
         });
-      }
+    }
 
     onSubmit() {
         this.showError = true;
         const formValue = this.tempForm.value;
-            this.showError = false;
-            this.tempForm.reset();
+        this.insert(formValue.temperature);
+        this.showError = false;
+        this.tempForm.reset();
     }
 
 
     insert(num: number): any {
-          if (num > 0 && num < 150) {
-              this.temperatureData.push(num);
-              this.min = Math.min.apply(null, this.temperatureData);
-              this.max = Math.max.apply(null, this.temperatureData);
-              this.mean = Number(this.meanTemperature().toFixed(2));
-              this.mode = this.calculateMode();
-              return this.temperatureData;
-            }
-            else {
-              return 'Temperature should be integer';
-            }
-          }
-        
-      
+        if (num > 0 && num < 150) {
+            this.temperatureData.push(num);
+            this.min = Math.min.apply(null, this.temperatureData);
+            this.max = Math.max.apply(null, this.temperatureData);
+            this.mean = Number(this.meanTemperature().toFixed(2));
+            this.mode = this.calculateMode();
+            return this.temperatureData;
+        }
+        else {
+            return 'Temperature should be integer';
+        }
+    }
 
-      meanTemperature(): number {
+
+
+    meanTemperature(): number {
         let totalSum = 0;
         this.temperatureData.forEach((element) => {
-          totalSum += element;
+            totalSum += element;
         });
         return totalSum / this.temperatureData.length;
-      }
+    }
 
 
-      calculateMode(): number {
+    calculateMode(): number {
         let mode = 0;
         for (let i = 0; i < this.temperatureData.length; i++) {
-          for (let j = 0; j < i; j++) {
-            if (this.temperatureData[j] === this.temperatureData[i]) {
-              mode = this.temperatureData[j];
+            for (let j = 0; j < i; j++) {
+                if (this.temperatureData[j] === this.temperatureData[i]) {
+                    mode = this.temperatureData[j];
+                }
             }
-          }
         }
         return mode;
-      }
-
-      getMin(): number {
-        return this.min;
-      }
-      getMax(): number {
-        return this.max;
-      }
-      getMean(): number {
-        return this.mean;
-      }
-      getMode(): number {
-        return this.mode;
-      }
     }
+
+    getMin(): number {
+        return this.min;
+    }
+    getMax(): number {
+        return this.max;
+    }
+    getMean(): number {
+        return this.mean;
+    }
+    getMode(): number {
+        return this.mode;
+    }
+}
